f4afa65953bb0fced20a816c833fc54d88ac1012
jckli <me@jackli.dev>
2025-11-09 00:17:06 +0000

    fix: add user agent to fix something, doesnt work l;ol

--- pximg/src/pximg.ts
+++ pximg/src/pximg.ts
@@ -2,6 +2,7 @@ export async function pximgProxy(pathname: string): Promise<Response> {
 	return fetch("https://i.pximg.net/" + pathname, {
 		headers: {
 			Referer: "https://www.pixiv.net",
+			UserAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:146.0) Gecko/20100101 Firefox/146.0",
 		},
 	});
 }
